dc474f8bb7acef3fb385870ad6db91b067b626fe
Rodolfo Castillo <raindeerdeveloper+dev@gmail.com>
2024-02-13 16:45:40 -0600

    Initial Commit

---
+++ LifeCycle/LifeCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCycleApp.swift
+//  LifeCycle
+//
+//  Created by Rodolfo Castillo on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LifeCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
